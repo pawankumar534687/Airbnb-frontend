48bd810da17cc10d77e73750f206076ba04c438c
pawankumar534687 <pawankumar534687@gmail.com>
2025-05-09 14:03:58 -1200

    fix: removed index.css import

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,63 @@
-import React from 'react'
-import './App.css'
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './components/Home'
-import { Route, Routes } from 'react-router-dom';
-import CreatePost from './components/CreatePost';
-import DetailedPost from './components/DetailedPost';
-import PostEdit from './components/PostEdit';
-import Signup from './components/Signup';
-import Login from './components/Login';
-import ProtectedRoute from './components/ProtectedRoute';
-import Logout from './components/Logout';
-import TokenDecode from './components/TokenDecode';
-
-
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import { Route, Routes } from "react-router-dom";
+import CreatePost from "./components/CreatePost";
+import DetailedPost from "./components/DetailedPost";
+import PostEdit from "./components/PostEdit";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Logout from "./components/Logout";
+import TokenDecode from "./components/TokenDecode";
 
 export const isAuthenticated = () => {
   return !!localStorage.getItem("token");
 };
 
 function App() {
- 
-  
-  
- 
-
   return (
     <>
-    
-    <TokenDecode>
-   
-      <Navbar isAuthenticated={isAuthenticated()}/>
-      <Routes>
-        <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute> }/>
-        <Route path="/create-post"  element={<ProtectedRoute><CreatePost/></ProtectedRoute>}/>
-        <Route path="/detailedpost/:id" element={<DetailedPost/>}/>
-        <Route path="/edit/:id" element={<ProtectedRoute><PostEdit/></ProtectedRoute>}/>
+      <TokenDecode>
+        <Navbar isAuthenticated={isAuthenticated()} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/create-post"
+            element={
+              <ProtectedRoute>
+                <CreatePost />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/detailedpost/:id" element={<DetailedPost />} />
+          <Route
+            path="/edit/:id"
+            element={
+              <ProtectedRoute>
+                <PostEdit />
+              </ProtectedRoute>
+            }
+          />
 
-      <Route path='/signup' element={<Signup/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/logout' element={<Logout/>}/>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
 
-      </Routes>
-      
-      <Footer/>
-     
+        <Footer />
       </TokenDecode>
-     
     </>
-  )
+  );
 }
 
 export default App;
