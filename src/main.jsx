e81d907d18e74d0abb96992ddc706acb5beb423b
pawankumar534687 <pawankumar534687@gmail.com>
2025-05-09 14:48:24 -1200

    Removed unused style.css

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
